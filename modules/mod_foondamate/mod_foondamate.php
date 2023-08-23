502dafae76ef63363f718fd0783125f7ee77f1ce
Arthur Edmond Jenkins <deesrex@fox5.co.za>
2023-08-23 12:46:04 +0200

    Your commit message here

--- modules/mod_foondamate/mod_foondamate.php
+++ modules/mod_foondamate/mod_foondamate.php
@@ -12,7 +12,20 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Helper\ModuleHelper;
 
-$list  = \Rex\Module\Foondamate\Site\Helper\FoondamateHelper::getList();
+$equations = [
+    "5x + 3 = 2x - 7",
+    "3x - 4 = 11",
+    "7x = 21",
+    "x + 10 = 6",
+    "9x - 4 = 5x + 6",
+    "4x + 2 = 4",
+    "6 - 2x = 3x + 4",
+    "7x - 3 = 4x + 2",
+    "8x = 8",
+    "5 - x = 3x + 2"
+];
+
+$list  = \Rex\Module\Foondamate\Site\Helper\FoondamateHelper::getList($equations[4]);
 $rexHelper = new \Rex\Module\Foondamate\Site\Helper\FoondamateHelper();
 
 require ModuleHelper::getLayoutPath('mod_foondamate', $params->get('layout', 'default'));
